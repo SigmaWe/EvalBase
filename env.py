882520f569d828589de98415a3accdcacbdbc25a
Ruixuan Tu <turx2003@gmail.com>
2022-10-14 20:32:17 -0500

    init with env, eval, newsroom

---
+++ env.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("/Users/turx/Projects/Research/dar/DocAsRef/")
+import bertscore_sentence.eval as bertscore_sentence
+
+models = ["rouge", "bleurt", "bertscore", "bertscore-sentence"]
+datasets = {
+    "newsroom": {
+        "human_metrics": ["InformativenessRating", "RelevanceRating", "CoherenceRating", "FluencyRating"],
+        "docID_column": "ArticleID",
+        "document_column": "ArticleText",
+        "system_summary_column": "SystemSummary",
+        "reference_summary_column": "ReferenceSummary",
+        "approaches": ["trad", "new"],
+        "human_eval_only_path": "dataloader/newsroom-human-eval.csv",
+        "human_eval_w_refs_path": "dataloader/newsroom_human_eval_with_refs.csv",
+        "refs_path": "/media/forrest/12T_EasyStore1/data/NLP/resources/newsroom/test.jsonl"
+    },
+    "realsumm_abs": {
+        "human_metrics": ["litepyramid_recall"],
+        "approaches": ["trad", "new"]
+    },
+    "realsumm_ext": {
+        "human_metrics": ["litepyramid_recall"],
+        "approaches": ["trad", "new"]
+    },
+    "summeval": {
+        "human_metrics": ["consistency", "relevance", "coherence", "fluency"],
+        "reference_summary_column": "ReferenceSummary_0",  # the id ranges from 0 to 10
+        "approaches": ["trad", "new"]
+    },
+    "tac2010": {
+        "human_metrics": ["Pyramid", "Linguistic", "Overall"],
+        "approaches": ["new"],
+        "docID_column": "docsetID"
+    }
+}
+eval_metrics = ["rouge1", "rouge2", "rougeL", "rougeLsum", "bertscore", "bleurt", "bertscore-sentence"]
+corr_metrics = ["pearsonr", "kendalltau", "spearmanr"]
---
+++ eval.py
@@ -0,0 +1,4 @@
+import newsroom
+
+if __name__ == '__main__':
+    newsroom.main()
